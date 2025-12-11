143301b62b2838f2afeb7be0c07f1d8df871c66c
Otavio Santana <otaviopolianasantana@gmail.com>
2025-12-11 17:40:38 +0000

    feat: remove unused imports from NoSQLAPITest

--- src/test/java/org/soujava/demos/mongodb/document/NoSQLAPITest.java
+++ src/test/java/org/soujava/demos/mongodb/document/NoSQLAPITest.java
@@ -17,8 +17,6 @@ import org.jboss.weld.junit5.auto.AddExtensions;
 import org.jboss.weld.junit5.auto.AddPackages;
 import org.jboss.weld.junit5.auto.EnableAutoWeld;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
